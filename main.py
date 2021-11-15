873d10949298afcc3513435423e92f494a7de054
unknown <clementrobin@fastmail.fm>
2021-11-15 20:29:24 +0000

    threading changes

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
-from Market import Market
+from market_class import Market
+import threading
+import time
+import numpy
 
-markets_to_track = ["pfe", "ibm"]
+with open("markets_to_track.txt", "r") as ffile:
+    markets_to_track = ffile.read().splitlines() 
 
 tracked_markets = [Market(market) for market in markets_to_track]
+all_threads = [threading.Thread(target=m.getHistorical1Day) for m in tracked_markets]
 
-for m in tracked_markets:
-    print(m.market)
-    m.update()
-    print(m.RSI1Day)
+# Start all the http reqs
+for t in all_threads:
+    print("Starting thread", t)
+    t.start()
+
+# Wait for all threads to finish before continuing
+for t in all_threads:
+    print("Joining", t)
+    t.join()
+
+
+def get_all_live_data(tracked_markets):
+    all_t = [threading.Thread(target=m.update) for m in tracked_markets]
+    for t in all_t:
+        t.start()
+    
+    for t in all_t:
+        t.join()
+
+    for m in tracked_markets:
+        print()
+        print(m.market)
+        print(m.RSI1Day)
+
+last_vals=None
+monitor = 1
+while monitor < 10:
+    get_all_live_data(tracked_markets)
+    curr_vals = [m.RSI1Day for m in tracked_markets]
+    if last_vals:
+        print("Diffs from last poll: ")
+        print(numpy.subtract(curr_vals, last_vals))
+        
+    last_vals = curr_vals
+
+    print("Slept 1s, going again!")
+    monitor += 2
+    time.sleep(2)
---
+++ market_class.py
@@ -0,0 +1,67 @@
+import requests
+import pandas as pd
+import ta.momentum
+import datetime
+
+class Market:
+    apikey = open("apikey.txt").readline()
+
+    def __init__(self, market, data=None):
+        self.market = market
+        if not data:
+            pd.DataFrame
+
+    def update(self):
+        """
+        Updates the market with all relevant info and calculates indicators
+        """
+        # self.getHistorical1Day()
+        self.getLivePrice()
+        self.getRSI1Day()
+
+    def getHistorical1Day(self):
+        # past 100 *TRADING* days data, actual count may be lower for days not traded like holidays
+        i = 100
+        print("Getting historical data")
+        startDate = datetime.date.today()
+        while (i > 0):
+            if (startDate.weekday() < 5):
+                i -= 1
+            startDate = startDate - datetime.timedelta(days=1)
+        endDate = datetime.date.today() - datetime.timedelta(days=1)
+
+        url = f"https://api.tiingo.com/iex/{self.market}/prices?startDate={startDate}" \
+              f"&resampleFreq=8hour&afterHours=false&forceFill=true&token={self.apikey}&endDate={endDate}"
+        x = requests.get(url)
+
+        df = pd.DataFrame(x.json())
+        df.drop(columns=df.columns[[2, 3, 4]], axis=1, inplace=True)
+        self.data = df
+
+    # rn this always appends new data, but sometimes we might only want one piece of data from current day (like in the
+    # case of RSI1Day)
+    def getLivePrice(self):
+        print("Getting live price")
+        url = f"https://api.tiingo.com/iex/?tickers={self.market}&token={self.apikey}"
+        x = requests.get(url)
+        df = pd.DataFrame(x.json())
+        df.drop(['low', 'ticker', 'bidPrice', 'bidSize', 'lastSaleTimestamp', 'prevClose', 'high', 'mid', 'volume',
+                 'open', 'quoteTimestamp', 'askPrice', 'askSize', 'lastSize', 'tngoLast'], axis=1, inplace=True)
+        df.rename(columns={'last': 'close', 'timestamp': 'date'}, inplace=True)
+
+        self.data = self.data.append(df, ignore_index=True)
+
+    def getRSI1Day(self):
+        print("Getting RSI")
+        df = self.data
+        prices = df['close'].squeeze()
+
+        # check for missing data
+        missing = 0
+        for x in prices:
+            if int(x) == 0:
+                missing = 1
+
+        if missing == 0:
+            rsis = ta.momentum.RSIIndicator(prices, 14)
+            self.RSI1Day = rsis.rsi().values[-1]
